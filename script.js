f1bddccc76aa3c106e82812bc20e02617624de54
dinitrogen <noshowdon@gmail.com>
2021-06-17 14:03:50 -0400

    Added basic AI mode. AI randomly picks.

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const gameBoard = (function() {
         return gameSquares[squareIndex];
     }
 
+    const getSquaresArray = function() {
+        return gameSquares;
+    }
+
     const resetSquares = function() {
         gameSquares = ["","","","","","","","",""];
         return gameSquares;
@@ -22,6 +26,7 @@ const gameBoard = (function() {
     return {
         setSquare,
         getSquare,
+        getSquaresArray,
         resetSquares,
     }
 })();
@@ -32,6 +37,7 @@ const displayController = (function() {
     const gameOutcome = document.querySelector('#gameOutcome');
     const gameSquareDivs = document.querySelectorAll('.gameSquare');
     const newGameButton = document.querySelector('#newGameButton');
+    const newAIGameButton = document.querySelector('#newAIGameButton');
     const replayButton = document.querySelector('#replayButton');
     const player1NameDiv = document.querySelector('#player1NameDiv');
     const player2NameDiv = document.querySelector('#player2NameDiv');
@@ -40,6 +46,7 @@ const displayController = (function() {
     let playerXturn = true;
     let player1;
     let player2;
+    let vsComputerMode = false;
     
     const updateDisplay = function() {
         for (let i = 0; i < gameSquareDivs.length; i++) {
@@ -136,6 +143,26 @@ const displayController = (function() {
         player2NameDiv.textContent = `${player2.getName()}:`;
         gameBoard.resetSquares();
         displayController.updateDisplay();
+        vsComputerMode = false;
+        playerXturn = true;
+        document.getElementById('playerCardO').style.backgroundColor = 'white';
+                document.getElementById('playerCardX').style.backgroundColor = 'rgb(196,196,196)';      
+        for (let i = 0; i < gameSquareDivs.length; i++) {
+            document.getElementById(i).style.pointerEvents = 'auto';
+        }    
+        document.getElementById('endGameOverlay').style.display = 'none';
+    });
+
+    newAIGameButton.addEventListener('click', function() {
+        let player1Name = prompt('Player 1 name?', 'Player 1');
+        let player2Name = 'Computer';
+        player1 = createPlayer(player1Name);
+        player2 = createPlayer(player2Name);
+        player1NameDiv.textContent = `${player1.getName()}:`;
+        player2NameDiv.textContent = `${player2.getName()}:`;
+        gameBoard.resetSquares();
+        displayController.updateDisplay();
+        vsComputerMode = true;
         playerXturn = true;
         document.getElementById('playerCardO').style.backgroundColor = 'white';
                 document.getElementById('playerCardX').style.backgroundColor = 'rgb(196,196,196)';      
@@ -152,6 +179,12 @@ const displayController = (function() {
                 playerXturn = false;
                 document.getElementById('playerCardO').style.backgroundColor = 'rgb(196,196,196)';
                 document.getElementById('playerCardX').style.backgroundColor = 'white';
+                if (vsComputerMode === true) {
+                    displayController.aiMove();
+                    playerXturn = true;
+                    document.getElementById('playerCardO').style.backgroundColor = 'white';
+                    document.getElementById('playerCardX').style.backgroundColor = 'rgb(196,196,196)';
+                }
             } else {
                 gameBoard.setSquare(e.id,"O");
                 playerXturn = true;
@@ -160,13 +193,26 @@ const displayController = (function() {
             }
             displayController.updateDisplay();
             displayController.checkForWin();
+            
         });
     });
+
+    const aiMove = function() {
+        if (gameBoard.getSquaresArray().includes("") === true) {   
+            let aiMoveIndex = Math.floor(Math.random() * 8);
+            if (gameBoard.getSquare(aiMoveIndex) === "") {
+                gameBoard.setSquare(aiMoveIndex, "O");
+            } else {
+                displayController.aiMove();
+            }
+        }
+    }
 
     return {
         updateDisplay,
         checkForWin,
         endRound,
+        aiMove,
     }
 })();
 
